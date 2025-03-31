08ba9f78130a4832790ae03d3eada217e09f2378
Tiago Dicquemare <tdicquemare@gmail.com>
2025-03-31 17:48:49 +0200

    calls working

--- composeApp/src/commonMain/kotlin/com/dicquemare/aiquizzgenerator/core/data/network/LocalSetupNetwork.kt
+++ composeApp/src/commonMain/kotlin/com/dicquemare/aiquizzgenerator/core/data/network/LocalSetupNetwork.kt
@@ -1,5 +1,5 @@
 package com.dicquemare.aiquizzgenerator.core.data.network
 
 object NetworkConfig {
-    const val LOCAL_IP = "10.28.1.206" // Change when using different wi-fi
+    const val LOCAL_IP = "http://$10.28.1.206:5001/api" // Change when using different wi-fi
 }
